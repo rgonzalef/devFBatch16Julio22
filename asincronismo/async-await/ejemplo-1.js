08714b7326d35dbf488bc3088d96dec36703f24b
kikemaya <enrique.mg.cd@gmail.com>
2022-07-21 16:25:47 -0500

    3era semana, clase 3

--- asincronismo/async-await/ejemplo-1.js
+++ asincronismo/async-await/ejemplo-1.js
@@ -1,13 +1,7 @@
 "use strict"
 
-// const getData = () => {
-//   return fetch('https://jsonplaceholder.typicode.com/users')
-//     .then(response => response.json())
-//     .then(data => data)
-//     .catch(error => console.error(error))
-// }
-
-const getData = async() => {
+// Ejemplo de async await, con function
+async function getData() {
     try {
       const response = await fetch('https://jsonplaceholder.typicode.com/users')
       const data = await response.json()
@@ -15,21 +9,26 @@ const getData = async() => {
     } catch (error) {
       console.error("Este es mi error: ", error);
     }
-}
-
-// flecha/arrow function
-// const showData = async () => {
-//   const data = await getData()
-//   return console.log(data)
-// }
-
-//async y await SIEMPRE van juntos
-//Sirve para esperar/await a que una promesa se resuelva y obtener los datos que retorna esa promesa
-//Es una funcion que me permite trabajar asincronia, como sincronia
+  }
+  
 async function showData() {
   const data = await getData()
   return console.log(data)
 }
 
-//NOTA: En funciones constructoras, SIEMPRE se utiliza 'this'
 showData()
+
+
+// Ejemplo con .then, .catch y de funcion async de tipo flecha
+// const getData = () => {
+//   return fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(data => data)
+//     .catch(error => console.error(error))
+// }
+  
+// flecha/arrow function
+// const showData = async () => {
+//   const data = await getData()
+//   return console.log(data)
+// }
